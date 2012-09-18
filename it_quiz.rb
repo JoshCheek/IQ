3c98cee7416f0a992560ccbc33cfc2c02ffbd431
Josh Cheek <josh.cheek@gmail.com>
2012-09-18 00:38:40 -0500

    Get basic functionality working
    Add Question and Answer classes, chang html and controllers
    slightly to reflect this

--- it_quiz.rb
+++ it_quiz.rb
@@ -1,13 +1,49 @@
 require 'sinatra'
 
+class Question
+  attr_reader :name, :answer
+
+  def initialize(name, answer)
+    @name, @answer = name, answer
+  end
+end
+
+class Answer
+  attr_reader :given
+
+  def initialize(given, question)
+    @given, @question = given, question
+  end
+
+  def actual
+    @question.answer
+  end
+end
+
+helpers do
+  def questions
+    @questions = Hash.new
+    [['a pen', 'una penna'],
+     ["a cat", "un gatto"],
+     ["hello", "salve"],
+    ].each do |name, answer|
+      @questions[name] = Question.new name, answer
+    end
+    @questions
+  end
+end
+
 get '/' do
   @title = 'Italian Quiz'
-  @questions = Hash.new
-  @questions["a pen"] = ["una penna"]
+  questions
   erb :quiz
 end
 
 post '/results' do
-    @title = 'Italian Quiz Results'
-    erb :results
+  @title = 'Italian Quiz Results'
+  @answers = {}
+  params[:questions].each do |name, answer|
+    @answers[name] = Answer.new answer, questions[name]
+  end
+  erb :results
 end
